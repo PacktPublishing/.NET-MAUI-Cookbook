cade5bce04a0229b7bb7e01caf584d37097bf226
Alexgoon <alexander.russkov@devexpreess.com>
2024-06-19 10:47:28 +0400

    chapter 3

---
+++ Chapter03/c3-CustomDrawing/c3-CustomDrawing/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace c3_DarkAndLightThemes {
+    public partial class AppShell : Shell {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Chapter03/c3-ThemedImagesAndStatusBar/c3-ThemedImagesAndStatusBar/ThemeSettings.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c3_DarkAndLightThemes {
+    public partial class ThemeInfo {
+        public static ThemeInfo System = new(AppTheme.Unspecified, "System");
+        public static ThemeInfo Light = new(AppTheme.Light, "Light");
+        public static ThemeInfo Dark = new(AppTheme.Dark, "Dark");
+        public AppTheme AppTheme { get; }
+        public string Caption { get; }
+
+        public ThemeInfo(AppTheme theme, string caption) {
+            AppTheme = theme;
+            Caption = caption;
+        }
+    }
+    public partial class ThemeSettings : ObservableObject {
+        public static List<ThemeInfo> ThemesList { get; } = new List<ThemeInfo>() {
+            ThemeInfo.System,
+            ThemeInfo.Light,
+            ThemeInfo.Dark
+        };
+        public static ThemeSettings Current { get; } = new ThemeSettings();
+
+        [ObservableProperty]
+        public ThemeInfo selectedTheme = ThemesList.First();
+
+        partial void OnSelectedThemeChanged(ThemeInfo oldValue, ThemeInfo newValue) {
+            Application.Current.UserAppTheme = newValue.AppTheme;
+        }
+    }
+}
